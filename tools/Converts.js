bcb5f733367fa20702c5c2f93613a73a36715920
justwiner <392110917@qq.com>
2018-07-26 09:20:40 +0800

    添加进制转换方法

--- tools/Converts.js
+++ tools/Converts.js
@@ -1,7 +1,11 @@
-let HexConvert = (() => {
+let Converts = (() => {
   const Stack = require('../lib/Stack')
-  class HexConvert {
-    baseConverter (decNumber, base) {
+  class Converts {
+    /**
+     * @param  {} decNumber 进制转换的源数据
+     * @param  {} base 需要转换的进制类型
+     */
+    static baseConverter (decNumber, base) {
       let remStack = new Stack(),
         rem,
         baseString = '',
@@ -11,14 +15,16 @@ let HexConvert = (() => {
         remStack.push(rem)
         decNumber = Math.floor(decNumber / base)
       }
-
       while(!remStack.isEmpty()){
         baseString += digits[remStack.pop()]
       }
       return baseString
     }
+    static moneyConverter (money, digitNum = 2) {
+      digitNum = (digitNum >= 0 && digitNum <= 20) ? digitNum : 2
+    }
   }
-  return HexConvert
+  return Converts
 })()
 
-exports = module.exports = HexConvert
+exports = module.exports = Converts
